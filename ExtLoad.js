053a4f8639e41e82dc5c8af1c43c7ec87007960d
Wish <100239673+wish13yt@users.noreply.github.com>
2025-01-05 13:14:55 -0600

    g

---
+++ ExtLoad.js
@@ -0,0 +1,3 @@
+const names = ["AntiVirus Pro", "ScamDetect", "GiftCard ScamFinder", "TackleIt Scam Guard", "TechAssist"];
+console.log("ExtLoad has successfully been loaded.")
+console.log(names[Math.floor(Math.random() * names.length)] + " has successfully been loaded. | Powered by ExtLoad")
